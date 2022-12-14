7a724847d757abcba033f126c1dad5b1f57edce8
Samuel-Obinna <nwasamobi@gmail.com>
2022-12-14 05:25:53 -0800

    I dun Tire

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints  of a quote in the standard
+ * Return: 1
+*/
+ int main(void)
+{
+	fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n stderr");
+	return (1);
+ } 
