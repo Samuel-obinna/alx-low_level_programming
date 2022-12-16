37cf1282ba17244772ee03f27f9351f214b3e9a1
Samuel-Obinna <nwasamobi@gmail.com>
2022-12-16 03:45:30 -0800

    Holy

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_alphabet -  Print alphabet in lower case
+ * Code written by Sazzy
+*/
+
+void print_alphabet(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+	_putchar('A');
+
+
+	_putchar('\n');
+}
