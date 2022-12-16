85ee23cd5007c44651f536d3b3b8b13df4267daa
Samuel-Obinna <nwasamobi@gmail.com>
2022-12-16 11:36:00 -0800

    on

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Code written by Sazzy
+*/
+
+void jack_bauer(void)
+{
+	int h, m;
+
+/*h is Hours, m is Minutes*/
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
