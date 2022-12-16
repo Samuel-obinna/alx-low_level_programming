4940c90f443e41df59fedf65cbfb1aa17af2e845
Samuel-Obinna <nwasamobi@gmail.com>
2022-12-16 10:06:16 -0800

    Mwxt

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _abs - Compute absolute value of integers
+ * Code written by Sazzy
+ * @n: Integer to be computed
+ * Return: The absolute value of integers
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+
+	else
+		return (-n);
+}
