03633f6f48ae1b44941e4abd73bccfaee2e1e19a
Samuel-Obinna <nwasamobi@gmail.com>
2022-12-16 19:41:09 -0800

    ad

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * add - Print sum of two  numbers
+ * Code written by Sazzy
+ * @a: The number to be added
+ * @b: The number to be added
+ * Return: The sum
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	_putchar(sum + '0');
+	return (sum);
+}
